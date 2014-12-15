1927bc888a7ac57532abf8a972b1e2c28ef8d084
diutsu <sousago@gmail.com>
2014-12-15 13:52:06 +0000

    adds mutation an recombination to lab 3

--- app/scripts/controllers/bcl03.js
+++ app/scripts/controllers/bcl03.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * 
+ *
  * www.cise.ufl.edu/research/sparse/matrices/Newman/karate.html@ngdoc function
  * @name bcBootstrapApp.controller:BcLabCtrl
  * @description
@@ -13,10 +13,17 @@ angular.module('bcBootstrapApp')
   $scope.acid = ['A','T','G','C'];
   $scope.randomSize = 10;
   $scope.populationSize = 5;
+  $scope.rr = .4;
+  $scope.mr = .3;
+  $scope.rfl = 4;
+  $scope.generationsNumber = 4;
+
+
+
   $scope.randomSeq = function(){
     var rndSeq = "";
     for(var i = 0; i < $scope.randomSize; i++){
-      rndSeq = rndSeq.concat($scope.acid[ Math.floor(Math.random()*($scope.acid.length-1))])
+      rndSeq = rndSeq.concat($scope.acid[ Math.floor(Math.random()*($scope.acid.length))])
     }
     $scope.fasta = "";
     for(var i = 0; i < $scope.populationSize; i++){
@@ -26,6 +33,8 @@ angular.module('bcBootstrapApp')
   }
 
   $scope.evolve = function(){
+            $scope.initTime = Date.now();
+
     var fastaArray = $scope.fasta.split("\n");
     $scope.fastaArray = fastaArray;
     $scope.names = new Array($scope.populationSize-1);
@@ -39,6 +48,59 @@ angular.module('bcBootstrapApp')
         $scope.sequences[Math.floor(i/3)] = fastaArray[i];
       }
     }
+   var logtxt =""
+   var sequences = $scope.sequences;
+    for(var gen = 0; gen < $scope.generationsNumber; gen++){
+        //mutate
+        for(var i = 0; i < sequences.length; i++){
+          if (Math.random() <= $scope.mr){
+            var pos = Math.floor(Math.random()*$scope.randomSize);
+
+            var oldValPos =  0
+            for(var j=0; j< $scope.acid.length; j++){
+              if(sequences[i][pos]== $scope.acid[j]){
+                oldValPos = j;
+                break;
+              }
+            }
+
+            var newVal = Math.floor(Math.random()*($scope.acid.length-1));
+            if (newVal >= oldValPos){
+              newVal++;
+            }
+
+
+            var replacement = $scope.acid[newVal];
+            sequences[i] =  sequences[i].substr(0, pos) + replacement +  sequences[i].substr(pos+replacement.length);
+
+       //     logtxt = logtxt.concat("On generation").concat(gen).concat(", ").concat($scope.names[i]).concat(' position ').concat(pos).concat(' mutated ').concat($scope.acid[oldValPos]).concat(' to ').concat($scope.acid[newVal]).concat("\n");
+          }
+        }
+
+         //recombinate
+        for(var i = 0; i < sequences.length; i++){
+          console.log($scope.rr);
+          if (Math.random() <= $scope.rr){
+            var pos = Math.floor(Math.random()*($scope.randomSize-$scope.rfl));
+
+            var srcSeq = Math.floor(Math.random()*(sequences.length-1));
+            if (srcSeq >= i){
+              srcSeq++;
+            }
+
+            var replacement = sequences[srcSeq].substr(pos, pos+$scope.rfl);
+            sequences[i] =  sequences[i].substr(0, pos) + replacement +  sequences[i].substr(pos+replacement.length);
+
+   //        logtxt = logtxt.concat("On generation").concat(gen).concat(", ").concat($scope.names[i]).concat(' position ').concat(pos).concat(' recombinated ').concat($scope.acid[oldValPos]).concat(' with ').concat($scope.names[srcSeq]).concat(", ").concat(sequences[srcSeq]).concat("\n");
+          }
+
+        }
+    //    logtxt = logtxt.concat(sequences).concat("\n");
+    }
+    $scope.logtxt = logtxt;
+        $scope.sequences = sequences;
+        $scope.totalTime = Date.now() - $scope.initTime;
+
   }
 }]);
 
