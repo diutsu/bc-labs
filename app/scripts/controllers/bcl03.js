e15f405c12f7bb371cbdcc2ad9c18c9b4c46d87d
diutsu <sousago@gmail.com>
2014-12-15 16:39:19 +0000

    Computed distances

--- app/scripts/controllers/bcl03.js
+++ app/scripts/controllers/bcl03.js
@@ -2,7 +2,7 @@
 
 /**
  *
- * www.cise.ufl.edu/research/sparse/matrices/Newman/karate.html@ngdoc function
+ * @ngdoc function
  * @name bcBootstrapApp.controller:BcLabCtrl
  * @description
  * # BcLabCtrl
@@ -11,12 +11,12 @@
 angular.module('bcBootstrapApp')
 .controller('BcLabMolPhCtrl', ['$scope','$http',function ($scope,$http) {
   $scope.acid = ['A','T','G','C'];
-  $scope.randomSize = 10;
-  $scope.populationSize = 5;
-  $scope.rr = .4;
-  $scope.mr = .3;
-  $scope.rfl = 4;
-  $scope.generationsNumber = 4;
+  $scope.randomSize = 100;
+  $scope.populationSize = 100;
+  $scope.rr = .01;
+  $scope.mr = .01;
+  $scope.rfl = 5;
+  $scope.generationsNumber = 5000;
 
 
 
@@ -33,24 +33,30 @@ angular.module('bcBootstrapApp')
   }
 
   $scope.evolve = function(){
-            $scope.initTime = Date.now();
+    $scope.initTime = Date.now();
 
     var fastaArray = $scope.fasta.split("\n");
     $scope.fastaArray = fastaArray;
     $scope.names = new Array($scope.populationSize-1);
     $scope.sequences = new Array($scope.populationSize-1);
-    $scope.sets = new Array($scope.populationSize-1);
+    $scope.hammingDist = new Array($scope.populationSize-1);
+    $scope.jukes = new Array($scope.populationSize-1);
+
     for(var i = 0; i< fastaArray.length; i++){
       if(i%3 == 0){
         $scope.names[Math.floor(i/3)] = fastaArray[i];
       }
       if(i%3 == 1){
-        $scope.sequences[Math.floor(i/3)] = fastaArray[i];
+
+        $scope.sequences[Math.floor(i/3)] = new Array($scope.randomSize);
+        for(var j = 0; j< $scope.randomSize;j++){
+          $scope.sequences[Math.floor(i/3)][j] = fastaArray[i][j];
+        }
       }
     }
-   var logtxt =""
-   var sequences = $scope.sequences;
-    for(var gen = 0; gen < $scope.generationsNumber; gen++){
+    // var logtxt =""
+     var sequences = $scope.sequences;
+      for(var gen = 0; gen < $scope.generationsNumber; gen++){
         //mutate
         for(var i = 0; i < sequences.length; i++){
           if (Math.random() <= $scope.mr){
@@ -58,7 +64,7 @@ angular.module('bcBootstrapApp')
 
             var oldValPos =  0
             for(var j=0; j< $scope.acid.length; j++){
-              if(sequences[i][pos]== $scope.acid[j]){
+              if(sequences[i][pos] == $scope.acid[j]){
                 oldValPos = j;
                 break;
               }
@@ -69,17 +75,14 @@ angular.module('bcBootstrapApp')
               newVal++;
             }
 
+            sequences[i][pos] =  $scope.acid[newVal];
 
-            var replacement = $scope.acid[newVal];
-            sequences[i] =  sequences[i].substr(0, pos) + replacement +  sequences[i].substr(pos+replacement.length);
-
-       //     logtxt = logtxt.concat("On generation").concat(gen).concat(", ").concat($scope.names[i]).concat(' position ').concat(pos).concat(' mutated ').concat($scope.acid[oldValPos]).concat(' to ').concat($scope.acid[newVal]).concat("\n");
+       //     logtxt = logtxt.concat("On generation ").concat(gen).concat(", ").concat($scope.names[i]).concat(' position ').concat(pos).concat(' mutated ').concat($scope.acid[oldValPos]).concat(' to ').concat($scope.acid[newVal]).concat("\n");
           }
         }
 
          //recombinate
         for(var i = 0; i < sequences.length; i++){
-          console.log($scope.rr);
           if (Math.random() <= $scope.rr){
             var pos = Math.floor(Math.random()*($scope.randomSize-$scope.rfl));
 
@@ -88,16 +91,42 @@ angular.module('bcBootstrapApp')
               srcSeq++;
             }
 
-            var replacement = sequences[srcSeq].substr(pos, pos+$scope.rfl);
-            sequences[i] =  sequences[i].substr(0, pos) + replacement +  sequences[i].substr(pos+replacement.length);
+            for(var j = 0; j< $scope.rfl;j++){
+              sequences[i][pos+j] =  sequences[srcSeq][pos+j];
+            }
 
-   //        logtxt = logtxt.concat("On generation").concat(gen).concat(", ").concat($scope.names[i]).concat(' position ').concat(pos).concat(' recombinated ').concat($scope.acid[oldValPos]).concat(' with ').concat($scope.names[srcSeq]).concat(", ").concat(sequences[srcSeq]).concat("\n");
+        //   logtxt = logtxt.concat("On generation ").concat(gen).concat(", ").concat($scope.names[i]).concat(' position ').concat(pos).concat(' recombinated ').concat($scope.acid[oldValPos]).concat(' with ').concat($scope.names[srcSeq]).concat(", ").concat(sequences[srcSeq]).concat("\n");
           }
 
         }
-    //    logtxt = logtxt.concat(sequences).concat("\n");
-    }
-    $scope.logtxt = logtxt;
+
+        //distances
+        $scope.hammingDist[gen] = 0;
+        $scope.jukes[gen]=0;
+        var jukesResults = 0
+        for(var i = 0; i<sequences.length;i++){
+          for(var j=i;j<sequences.length;j++){
+            var diff=0;
+            for(var n=0;n<$scope.randomSize;n++){
+              if(sequences[i][n] != sequences[j][n]){
+                diff++;
+              }
+            }
+            $scope.hammingDist[gen]+=diff;
+
+            var p = diff/$scope.randomSize;
+            if(p<0.75){
+              $scope.jukes[gen] =$scope.jukes[gen]+ -3 * Math.log(1-4*p/3)/4;
+              jukesResults++;
+            }
+          }
+        }
+        $scope.hammingDist[gen] = $scope.hammingDist[gen]/(sequences.length*(sequences.length-1)/2);
+        $scope.jukes[gen] = $scope.jukes[gen]/jukesResults;
+      //    logtxt = logtxt.concat(sequences).concat("\n");
+      }
+     // $scope.logtxt = logtxt;
+
         $scope.sequences = sequences;
         $scope.totalTime = Date.now() - $scope.initTime;
 
